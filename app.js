6904a6de9523b1972cf2d4ee39df95a0a5e3fe7a
unknown <07240044.sherubtse@rub.edu.bt>
2026-09-17 11:36:19 +0600

    Unit V Lab 6: client-side order lookup consuming the REST API

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Body parsing middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.json());
 
 // Routes
 const indexRoutes = require('./routes/index');
@@ -26,3 +27,4 @@ app.use('/', indexRoutes);
 app.listen(PORT, () => {
   console.log(`Campus Eats running at http://localhost:${PORT}`);
 });
+
